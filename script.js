f5e84b38ebbc9bfda96965bdc3aa825b06cd8e61
Peter McKasty <pmck25@yahoo.com>
2023-05-03 22:54:45 -0400

    added get local storage code

--- script.js
+++ script.js
@@ -1,13 +1,5 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
+
   $("#hour-9 .saveBtn").on("click", function () {
     let hourText9 = ""
     let textarea9 = "";
@@ -73,12 +65,6 @@ $(function () {
   })
 
 
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
   $(".time-block").each(function () {
     let currentTime = dayjs().hour()
     let timeBlock = parseInt($(this).attr("hour-9", "hour-10", "hour-11", "hour-12", "hour-13", "hour-14", "hour-15", "hour-16", "hour-17"));
@@ -90,12 +76,69 @@ $(function () {
         $(this).addClass("future");
     }
 });
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
 
-  //
-  // TODO: Add code to display the current date in the header of the page.
+  $(document).ready(function() {
+    let storedText9 = localStorage.getItem('hour-9');
+    if (storedText9) {
+      $('#hour-9 textarea').val(storedText9);
+    }
+  });
+
+  $(document).ready(function() {
+    let storedText10 = localStorage.getItem('hour-10');
+    if (storedText10) {
+    $('#hour-10 textarea').val(storedText10);
+    }
+  }); 
+
+  $(document).ready(function() {
+    let storedText11 = localStorage.getItem('hour-11');
+    if (storedText11) {
+    $('#hour-11 textarea').val(storedText11);
+    }
+  });
+
+  $(document).ready(function() {
+    let storedText12 = localStorage.getItem('hour-12');
+    if (storedText12) {
+      $('#hour-12 textarea').val(storedText12);
+    }
+  });
+
+  $(document).ready(function() {
+    let storedText13 = localStorage.getItem('hour-13');
+    if (storedText13) {
+      $('#hour-13 textarea').val(storedText13);
+    }
+  });
+
+  $(document).ready(function() {
+    let storedText14 = localStorage.getItem('hour-14');
+    if (storedText14) {
+      $('#hour-14 textarea').val(storedText14);
+    }
+  });
+
+  $(document).ready(function() {
+    let storedText15 = localStorage.getItem('hour-15');
+    if (storedText15) {
+      $('#hour-15 textarea').val(storedText15);
+    }
+  });
+
+  $(document).ready(function() {
+    let storedText16 = localStorage.getItem('hour-16');
+    if (storedText16) {
+    $('#hour-16 textarea').val(storedText16);
+    }
+  });
+
+  $(document).ready(function() {
+    let storedText17 = localStorage.getItem('hour-17');
+    if (storedText17) {
+    $('#hour-17 textarea').val(storedText);
+    }
+  });
 });
 
 let today = dayjs().format('MMM D, YYYY, hh:mm a')
