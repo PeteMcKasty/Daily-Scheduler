76ce971ae2c2017a6410b7f1206696ad37f99660
Peter McKasty <pmck25@yahoo.com>
2023-05-03 22:19:18 -0400

    debugged set local storage code

--- script.js
+++ script.js
@@ -10,48 +10,66 @@ $(function () {
   // useful when saving the description in local storage?
   $("#hour-9 .saveBtn").on("click", function () {
     let hourText9 = ""
+    let textarea9 = "";
+    textarea9 = $('#hour-9 textarea').val();
     hourText9 = $('#hour-9 .hour').text();
-    localStorage.setItem('hour-9', hourText9);
+    localStorage.setItem('hour-9', hourText9 + textarea9);
   })
   $("#hour-10 .saveBtn").on("click", function () {
     let hourText10 = ""
+    let textarea10 = "";
+    textarea10 = $('#hour-10 textarea').val();
     hourText10 = $('#hour-10 .hour').text();
-    localStorage.setItem('hour-10', hourText10);
+    localStorage.setItem('hour-10', hourText10 + textarea10);
   })  
   $("#hour-11 .saveBtn").on("click", function () {
     let hourText11 = ""
+    let textarea11 = "";
+    textarea11 = $('#hour-11 textarea').val();
     hourText11 = $('#hour-11 .hour').text();
-    localStorage.setItem('hour-11', hourText11);
+    localStorage.setItem('hour-11', hourText11 + textarea11);
   })
   $("#hour-12 .saveBtn").on("click", function () {
-    let hourText12 = ""
+    let hourText12 = "";
+    let textarea12 = "";
+    textarea12 = $('#hour-12 textarea').val();
     hourText12 = $('#hour-12 .hour').text();
-    localStorage.setItem('hour-12', hourText12);
+    localStorage.setItem('hour-12', hourText12 + textarea12);
   })
   $("#hour-13 .saveBtn").on("click", function () {
     let hourText13 = ""
+    let textarea13 = "";
+    textarea13 = $('#hour-13 textarea').val();
     hourText13 = $('#hour-13 .hour').text();
-    localStorage.setItem('hour-13', hourText13);
+    localStorage.setItem('hour-13', hourText13 + textarea13);
   })
   $("#hour-14 .saveBtn").on("click", function () {
     let hourText14 = ""
+    let textarea14 = "";
+    textarea14 = $('#hour-14 textarea').val();
     hourText14 = $('#hour-14 .hour').text();
-    localStorage.setItem('hour-14', hourText14);
+    localStorage.setItem('hour-14', hourText14 + textarea14);
   })
   $("#hour-15 .saveBtn").on("click", function () {
     let hourText15 = ""
+    let textarea15 = "";
+    textarea15 = $('#hour-15 textarea').val();
     hourText15 = $('#hour-15 .hour').text();
-    localStorage.setItem('hour-15', hourText15);
+    localStorage.setItem('hour-15', hourText15 + textarea15);
   })
   $("#hour-16 .saveBtn").on("click", function () {
     let hourText16 = ""
+    let textarea16 = "";
+    textarea16 = $('#hour-16 textarea').val();
     hourText16 = $('#hour-16 .hour').text();
-    localStorage.setItem('hour-16', hourText16);
+    localStorage.setItem('hour-16', hourText16 + textarea16);
   })
   $("#hour-17 .saveBtn").on("click", function () {
     let hourText17 = ""
+    let textarea17 = "";
+    textarea17 = $('#hour-17 textarea').val();
     hourText17 = $('#hour-17 .hour').text();
-    localStorage.setItem('hour-17', hourText17);
+    localStorage.setItem('hour-17', hourText17 + textarea17);
   })
 
 
